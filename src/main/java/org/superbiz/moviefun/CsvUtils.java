689d77f717fe7b57b33b2b84593827b8ff691634
gaijfox <jfox@gaig.com>
2018-10-17 15:59:36 -0600

    changed readiong of csv file for cloud

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -14,15 +14,14 @@ public class CsvUtils {
 
     public static String readFile(String path) {
         try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
-
+            Scanner scanner = new Scanner(CsvUtils.class.getClassLoader().getResourceAsStream(path)).useDelimiter("\\A");
             if (scanner.hasNext()) {
                 return scanner.next();
             } else {
                 return "";
             }
 
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
